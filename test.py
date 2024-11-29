974d82bb51a88b9488698503c4957188611949e3
idesho <keya_1110@yahoo.co.jp>
2024-11-30 01:51:12 +0900

    Update test.py

--- test.py
+++ test.py
@@ -134,6 +134,8 @@ options.add_argument('--no-sandbox')
 options.add_argument('--disable-dev-shm-usage')
 options.add_argument('--disable-gpu')
 options.add_argument('--window-size=1920x1080')
+options.add_argument('--ignore-certificate-errors')
+
 
 # URLリスト
 urls = {
