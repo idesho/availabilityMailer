50bb8560cb2363aa2d08f702a91388ea38bc4f94
idesho <keya_1110@yahoo.co.jp>
2025-05-27 23:54:44 +0900

    Update 東戸塚.py

--- 東戸塚.py
+++ 東戸塚.py
@@ -25,7 +25,7 @@ def get_weekday_jp(date):
 
 # Chrome options setup
 options = Options()
-# options.add_argument('--headless')
+options.add_argument('--headless')
 options.add_argument('--no-sandbox')
 options.add_argument('--disable-dev-shm-usage')
 options.add_argument('--disable-gpu')
